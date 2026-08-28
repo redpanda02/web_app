5bec82d5ba04474fdbc686c629b654d91e7f90d2
Maurice Varore <ndene154@gmail.com>
2026-08-28 15:26:37 +0000

    feat: server configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
-
 const app = express()
+
 const itemsRouter = require('./routes/item.routes')
 app.use(cors())
 app.use(express.json())
@@ -9,6 +10,7 @@ app.use(express.json())
 app.get('/api/health', (request, response) => {
   response.json({ status: 'ok' })
 })
-app.use('api/items', itemsRouter)
+
+app.use('/api/items', itemsRouter)
 
 module.exports = app
